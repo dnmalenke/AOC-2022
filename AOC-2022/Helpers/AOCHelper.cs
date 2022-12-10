347d31e3690a3c015664e7dbb7129dc7b78c3301
David Malenke <dnmalenke@gmail.com>
2022-12-10 01:11:11 -0600

    support saving and loading inputs

---
+++ AOC-2022/Helpers/AOCHelper.cs
@@ -0,0 +1,26 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using System.Net;
+
+namespace AOC_2022.Helpers
+{
+    public class AOCHelper
+    {
+        private readonly ILocalStorageService _lStorage;
+
+        public AOCHelper(ILocalStorageService lStorage)
+        {
+            _lStorage = lStorage;
+        }
+
+        public async Task<string> LoadInput(int day, int slot)
+        {
+            return await _lStorage.GetItemAsync<string>($"d{day}s{slot}");
+        }
+
+        public async Task SaveInput(int day, int slot, string input)
+        {
+            await _lStorage.SetItemAsync($"d{day}s{slot}", input);
+        }
+    }
+}
--- AOC-2022/Program.cs
+++ AOC-2022/Program.cs
@@ -1,4 +1,7 @@
 using AOC_2022;
+using AOC_2022.Helpers;
+using Blazored.LocalStorage;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +10,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<AOCHelper>();
 
 await builder.Build().RunAsync();

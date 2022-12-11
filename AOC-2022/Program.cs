0f1812207fa3e074b3bf24d472e399025baafa13
David Malenke <dnmalenke@gmail.com>
2022-12-10 20:00:53 -0600

    don't use top level statements

--- AOC-2022/Program.cs
+++ AOC-2022/Program.cs
@@ -1,18 +1,23 @@
 using AOC_2022;
 using AOC_2022.Helpers;
 using Blazored.LocalStorage;
-
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddBlazoredLocalStorage();
+        builder.Services.AddBlazoredLocalStorage();
 
-builder.Services.AddScoped<AOCHelper>();
+        builder.Services.AddScoped<AOCHelper>();
 
-await builder.Build().RunAsync();
+        await builder.Build().RunAsync();
+    }
+}
